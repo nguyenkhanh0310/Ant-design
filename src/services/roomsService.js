8773131ec04e681f09c1502088823890519b64d0
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2024-10-28 22:38:08 +0700

    updateRoom api

--- src/services/roomsService.js
+++ src/services/roomsService.js
@@ -1,16 +1,21 @@
-import { del, get, post } from "../utils/request"
+import { del, get, patch, post } from "../utils/request"
 
 export const createRoom = async (options) => {
   const result = await post("rooms", options)
   return result;
 }
 
-export const getListRoom = async() => {
+export const getListRoom = async () => {
   const result = await get("rooms");
   return result;
 }
 
-export const deleteRoom = async(id) => {
+export const deleteRoom = async (id) => {
   const result = await del(`rooms/${id}`);
+  return result;
+}
+
+export const updateRoom = async (id, options) => {
+  const result = await patch(`rooms/${id}`, options);
   return result;
 }
